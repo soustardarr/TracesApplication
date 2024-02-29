e95d3c0279794882a80f37b184d73605515a5a8a
Ruslan Kozlov <ruslankozlov@MacBook-Pro-Ruslan.local>
2024-02-29 22:04:23 +0300

    first_commit

---
+++ Traces/Traces/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traces
+//
+//  Created by Ruslan Kozlov on 29.02.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
